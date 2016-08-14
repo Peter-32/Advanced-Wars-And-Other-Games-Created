0182503e23e6e26d224624f6a36a05fd9af3267c
Peter-32 <peter@impactradius.com>
2016-08-14 14:59:48 -0700

    nearly has basic drawing

--- Advanced_Wars/src/GameBoard.java
+++ Advanced_Wars/src/GameBoard.java
@@ -22,6 +22,101 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
  */
 public class GameBoard extends JFrame {
 
+    //// GETTERS AND SETTERS
+
+    public ImageIcon getResizedMountainIcon() {
+        return resizedMountainIcon;
+    }
+
+    public ImageIcon getResizedRoadTurnULIcon() {
+        return resizedRoadTurnULIcon;
+    }
+
+    public ImageIcon getResizedRoadTurnURIcon() {
+        return resizedRoadTurnURIcon;
+    }
+
+    public ImageIcon getResizedRoadTurnDLIcon() {
+        return resizedRoadTurnDLIcon;
+    }
+
+    public ImageIcon getResizedRoadTurnDRIcon() {
+        return resizedRoadTurnDRIcon;
+    }
+
+    public ImageIcon getResizedRoadUpIcon() {
+        return resizedRoadUpIcon;
+    }
+
+    public ImageIcon getResizedRoadRightIcon() {
+        return resizedRoadRightIcon;
+    }
+
+    public ImageIcon getResizedGrassIcon() {
+        return resizedGrassIcon;
+    }
+
+    public boolean isGameOver() {
+        return GameOver;
+    }
+
+    public void setGameOver(boolean gameOver) {
+        GameOver = gameOver;
+    }
+
+    public int getxClicked() {
+        return xClicked;
+    }
+
+    public void setxClicked(int xClicked) {
+        this.xClicked = xClicked;
+    }
+
+    public int getyClicked() {
+        return yClicked;
+    }
+
+    public void setyClicked(int yClicked) {
+        this.yClicked = yClicked;
+    }
+
+    public int getMenuStartX() {
+        return menuStartX;
+    }
+
+    public int getMenuStartY() {
+        return menuStartY;
+    }
+
+    public int getMenuWidth() {
+        return menuWidth;
+    }
+
+    public int getMenuHeight() {
+        return menuHeight;
+    }
+
+    public int getMapStartX() {
+        return mapStartX;
+    }
+
+    public int getMapStartY() {
+        return mapStartY;
+    }
+    public int getLeftFrameBorderWidth() {
+        return leftFrameBorderWidth;
+    }
+    public int getTitleBarHeight() {
+        return titleBarHeight;
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+
     //// FIELDS
 
     private enum TerrainTiles {
@@ -32,16 +127,18 @@ public class GameBoard extends JFrame {
     private final int height = 800;
     private final int titleBarHeight = 30;
     private final int leftFrameBorderWidth = 8;
+    private final int tileLength = 75;
+    private final int menuStartX = 125;
+    private final int menuStartY = 50;
+    private final int menuWidth = 250;
+    private final int menuHeight = 150;
+    private final int mapStartX = 450;
+    private final int mapStartY = 225;
 
     SpriteSheet ssBuildings;
     SpriteSheet ssUnits;
-    private int menuStartX = 125;
-    private int menuStartY = 50;
-    private int menuWidth = 250;
-    private int menuHeight = 150;
-    private int mapStartX = 450;
-    private int mapStartY = 225;
-    private int tileLength = 75;
+
+
     private boolean lockInput = false; // not sure if this still be used
     private int yClicked = -1;
     private int xClicked = -1;
@@ -49,47 +146,53 @@ public class GameBoard extends JFrame {
 
     // Sprites and enums
 
-    private enum terrainTile {
+    private enum TerrainTile {
         MOUNTAIN, GRASS,
         ROAD_VERTICAL, ROAD_HORIZONTAL, ROAD_TURN_UL, ROAD_TURN_UR, ROAD_TURN_DL, ROAD_TURN_DR
     }
-    private enum buildingTile {
+    private enum BuildingTile {
         GRAY_HQ, GRAY_BASE, GRAY_CITY,
         RED_HQ, RED_BASE, RED_CITY,
         BLUE_HQ, BLUE_BASE, BLUE_CITY
     }
-    private terrainTile[][] terrainTiles = new terrainTile[16][10];
-    private buildingTile[][] buildingTiles = new buildingTile[16][10];
+    private TerrainTile[][] terrainTilesGrid = new TerrainTile[16][10];
+    private BuildingTile[][] buildingTilesGrid = new BuildingTile[16][10];
 
-    ImageIcon mountainIcon = new ImageIcon("resources/mountain.png");
-    ImageIcon roadTurnULIcon = new ImageIcon("resources/road_turn_ul.png");
-    ImageIcon roadTurnURIcon = new ImageIcon("resources/road_turn_ur.png");
-    ImageIcon roadTurnDLIcon = new ImageIcon("resources/road_turn_dl.png");
-    ImageIcon roadTurnDRIcon = new ImageIcon("resources/road_turn_dr.png");
-    ImageIcon roadUpIcon = new ImageIcon("resources/road_up.png");
-    ImageIcon roadRightIcon = new ImageIcon("resources/road_right.png");
-    ImageIcon grassIcon = new ImageIcon("resources/grass.png");
+    private ImageIcon mountainIcon = new ImageIcon("resources/mountain.png");
+    private ImageIcon roadTurnULIcon = new ImageIcon("resources/road_turn_ul.png");
+    private ImageIcon roadTurnURIcon = new ImageIcon("resources/road_turn_ur.png");
+    private ImageIcon roadTurnDLIcon = new ImageIcon("resources/road_turn_dl.png");
+    private ImageIcon roadTurnDRIcon = new ImageIcon("resources/road_turn_dr.png");
+    private ImageIcon roadUpIcon = new ImageIcon("resources/road_up.png");
+    private ImageIcon roadRightIcon = new ImageIcon("resources/road_right.png");
+    private ImageIcon grassIcon = new ImageIcon("resources/grass.png");
 
-    Image mountainImage = mountainIcon.getImage();
-    Image roadTurnULImage = roadTurnULIcon.getImage();
-    Image roadTurnURImage = roadTurnURIcon.getImage();
-    Image roadTurnDLImage = roadTurnDLIcon.getImage();
-    Image roadTurnDRImage = roadTurnDRIcon.getImage();
-    Image roadUpImage = roadUpIcon.getImage();
-    Image roadRightImage = roadRightIcon.getImage();
-    Image grassImage = grassIcon.getImage();
+    private Image mountainImage = mountainIcon.getImage();
+    private Image roadTurnULImage = roadTurnULIcon.getImage();
+    private Image roadTurnURImage = roadTurnURIcon.getImage();
+    private Image roadTurnDLImage = roadTurnDLIcon.getImage();
+    private Image roadTurnDRImage = roadTurnDRIcon.getImage();
+    private Image roadUpImage = roadUpIcon.getImage();
+    private Image roadRightImage = roadRightIcon.getImage();
+    private Image grassImage = grassIcon.getImage();
 
-    Image resizedMountainImage = mountainImage.getScaledInstance(tileLength, (int) (tileLength * 1.2), java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadTurnULImage = roadTurnULImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadTurnURImage = roadTurnURImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadTurnDLImage = roadTurnDLImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadTurnDRImage = roadTurnDRImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadUpImage = roadUpImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedRoadRightImage = roadRightImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
-    Image resizedGrassImage = grassImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedMountainImage = mountainImage.getScaledInstance(tileLength, (int) (tileLength * 1.2), java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadTurnULImage = roadTurnULImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadTurnURImage = roadTurnURImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadTurnDLImage = roadTurnDLImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadTurnDRImage = roadTurnDRImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadUpImage = roadUpImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedRoadRightImage = roadRightImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
+    private Image resizedGrassImage = grassImage.getScaledInstance(tileLength, tileLength, java.awt.Image.SCALE_SMOOTH);
 
-
-
+    private ImageIcon resizedMountainIcon = new ImageIcon(resizedMountainImage);
+    private ImageIcon resizedRoadTurnULIcon = new ImageIcon(resizedRoadTurnULImage);
+    private ImageIcon resizedRoadTurnURIcon = new ImageIcon(resizedRoadTurnURImage);
+    private ImageIcon resizedRoadTurnDLIcon = new ImageIcon(resizedRoadTurnDLImage);
+    private ImageIcon resizedRoadTurnDRIcon = new ImageIcon(resizedRoadTurnDRImage);
+    private ImageIcon resizedRoadUpIcon = new ImageIcon(resizedRoadUpImage);
+    private ImageIcon resizedRoadRightIcon = new ImageIcon(resizedRoadRightImage);
+    private ImageIcon resizedGrassIcon = new ImageIcon(resizedGrassImage);
 
     // sound files
 
@@ -138,6 +241,10 @@ public class GameBoard extends JFrame {
         }
         ssBuildings = new SpriteSheet(spriteSheetBuildings);
         ssUnits = new SpriteSheet(spriteSheetUnits);
+
+        // load map1
+
+        loadMap1();
 
         // Start the mouse listener class
         // his prompts the main game loop each time there is user input
@@ -237,8 +344,29 @@ public class GameBoard extends JFrame {
         public void mouseExited(MouseEvent e) {
 
         }
+    } // END OF MouseListener INNER CLASS
+
+    void loadMap1() {
+
+        /*private enum TerrainTile {
+            MOUNTAIN, GRASS,
+            ROAD_VERTICAL, ROAD_HORIZONTAL, ROAD_TURN_UL, ROAD_TURN_UR, ROAD_TURN_DL, ROAD_TURN_DR
+        }
+        */
+
+        // default to all grass to start
+
+        for (TerrainTile[] row : terrainTilesGrid) {
+            for (int j = 0; j < row.length; j++) {
+                row[j] = TerrainTile.GRASS;
+            }
+        }
+
     }
-}
+
+
+
+} // END OF GameBoard CLASS
 
 class GameDrawingPanel extends JComponent {
 
@@ -298,12 +426,4 @@ class MainGameLoop implements Runnable {
 
 
     }
-}
-
-class TerrainTile {
-
-}
-
-class BuildingTile {
-
 }
---
+++ Advanced_Wars/src/tests.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Peter on 8/14/2016.
+ */
+public class tests {
+
+    private enum TerrainTile {
+        MOUNTAIN, GRASS,
+        ROAD_VERTICAL, ROAD_HORIZONTAL, ROAD_TURN_UL, ROAD_TURN_UR, ROAD_TURN_DL, ROAD_TURN_DR
+    }
+    private enum BuildingTile {
+        GRAY_HQ, GRAY_BASE, GRAY_CITY,
+        RED_HQ, RED_BASE, RED_CITY,
+        BLUE_HQ, BLUE_BASE, BLUE_CITY
+    }
+    private TerrainTile[][] terrainTilesGrid = new TerrainTile[16][10];
+
+
+    public static void main(String[] args) {
+        new tests();
+    }
+
+    tests() {
+        for (TerrainTile[] row : terrainTilesGrid) {
+            for (int j = 0; j < row.length; j++) {
+                row[j] = TerrainTile.GRASS;
+                System.out.println(row.length);
+            }
+        }
+    }
+}
