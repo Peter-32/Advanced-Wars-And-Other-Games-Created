c43bb16492282fe0c4e9dfe119984a8d3a2c6a54
Peter-32 <peter@impactradius.com>
2016-08-21 17:30:40 -0700

    added IO for choosing a map

--- Advanced_Wars/src/RunProgram.java
+++ Advanced_Wars/src/RunProgram.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by Peter on 8/14/2016.
  */
 public class RunProgram {
     public static void main(String[] args) {
-        new GameBoard();
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> mapsStored = new ArrayList<Integer>(Arrays.asList(1));
+
+        System.out.println("Which map should be loaded?");
+        System.out.println("1: Mountain Pass");
+
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+        }
+        int mapNumber = scanner.nextInt();
+
+        if (!mapsStored.contains(mapNumber)) {
+            mapNumber = 1;
+        }
+
+        new GameBoard(mapNumber);
     }
 }
